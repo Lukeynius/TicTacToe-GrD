1aa10bd6beda274d83f31b9f5eb9bbf70194b4ff
Lukeynius <lukas.prager11@gmail.com>
2025-06-24 14:51:34 +0200

    add javafiles & projectstructure

---
+++ src/main/java/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+
+    //#########################################################################
+    // variables
+    private char marker;
+
+
+    //#########################################################################
+    // Constructor
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+}
