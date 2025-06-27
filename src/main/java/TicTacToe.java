3c7e103566fe2cae907622183db49b730da1a6d0
Lukeynius <lukas.prager11@gmail.com>
2025-06-27 23:15:15 +0200

    add functions isFull & hasWinner

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -102,8 +102,53 @@ public class TicTacToe {
         scanner.close();
     }
 
+
     // switch the current player
     private void switchCurrentPlayer() {
         currentPlayer = (currentPlayer == player1) ? player2 : player1;
+    }
+
+
+    // check if the current player has won
+    private boolean hasWinner() {
+        char m = currentPlayer.getMarker();
+
+        // win combinations
+        int[][][] kombis = {
+                {{0, 0}, {0, 1}, {0, 2}},
+                {{1, 0}, {1, 1}, {1, 2}},
+                {{2, 0}, {2, 1}, {2, 2}},
+                {{0, 0}, {1, 0}, {2, 0}},
+                {{0, 1}, {1, 1}, {2, 1}},
+                {{0, 2}, {1, 2}, {2, 2}},
+                {{0, 0}, {1, 1}, {2, 2}},
+                {{0, 2}, {1, 1}, {2, 0}}
+        };
+
+        for (int[][] kombi : kombis) {
+            int x1 = kombi[0][0], y1 = kombi[0][1];
+            int x2 = kombi[1][0], y2 = kombi[1][1];
+            int x3 = kombi[2][0], y3 = kombi[2][1];
+
+            if (!board.isCellEmpty(x1, y1) &&
+                    !board.isCellEmpty(x2, y2) &&
+                    !board.isCellEmpty(x3, y3)) {
+
+                // placing on an occupied field will fail therefore is a marker here
+                Board test = new Board();
+                test.clear();
+                test.place(x1, y1, m);
+                test.place(x2, y2, m);
+                test.place(x3, y3, m);
+
+                if (test.isCellEmpty(x1, y1) &&
+                        test.isCellEmpty(x2, y2) &&
+                        test.isCellEmpty(x3, y3)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
